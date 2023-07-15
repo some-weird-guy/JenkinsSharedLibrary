39e23f907a6d97a3e649f03e20eb06dda5954ea9
KAPIL KUMAR <78157878+some-weird-guy@users.noreply.github.com>
2023-07-15 22:37:40 +0530

    Update CauseUtils.groovy

--- src/utils/CauseUtils.groovy
+++ src/utils/CauseUtils.groovy
@@ -10,13 +10,14 @@ import hudson.triggers.TimerTrigger.TimerTriggerCause
 
 
 class CauseUtils {
+  
   def buildObj;
-
   def causeTypes = [0 : [_class : 'class hudson.model.Cause$UserIdCause', associatedCauses : [], haveAssociatedBuild : false],
                     1 : [_class : 'class com.sonyericsson.rebuild.RebuildCause', associatedCauses : [0], haveAssociatedBuild : true],
                     2 : [_class : 'class org.jenkinsci.plugins.workflow.cps.replay.ReplayCause', associatedCauses : [0], haveAssociatedBuild : true],
                     3 : [_class : 'class hudson.model.Cause$UpstreamCause', associatedCauses : [], haveAssociatedBuild : true ]
-
+                   ];
+  
   CauseUtils(def script, def buildObj) {
     this.script = script;
     this.buildObj = buildObj;
@@ -28,8 +29,7 @@ class CauseUtils {
       if(it.value['_class'] == causeClass) {
         return it.value
       }
-    }
-    
+    } 
   }
 
   @NonCPS
@@ -42,19 +42,28 @@ class CauseUtils {
   @NonCPS
   def _getBuildCausesFromBuildObj(def buildObj, def filter, def causeList, def currentLevel) {
 
+    def __currentLevelBuildObj = buildObj;
+     
     for(Action a : this._getCauseActionFromBuildObj()) {
       for(Cause c : a.getCauses()) {
         def causeDetails = [
           __causeClass : c.getClass(),
-          __buildExternalizableId : __currentLevelbuildObj.getExternalizableId()
-          _primary : [:],
-          _secondary : [:],
+          __buildExternalizableId : __currentLevelBuildObj.getExternalizableId()
+          _primary : null,
+          _secondary : null,
           __level : currentLevel,
+          ShortDescription : c.getShortDescription(),
           _childs : []
         ];
-        causeDetails['_primary']['ShortDescription'] = c.getShortDescription;
-        def causeTypeMetaInfo = __getCauseTypeMetaInfo(causeDetails["__causeClass"])
-        if(causeDetails["__causeClass"] == this.causeTypes["0"]["_class"]) {
+        def causeTypeMetaInfo = __getCauseTypeMetaInfo(causeDetails["__causeClass"]);
+        if(causeTypeMetaInfo["_class"] == this.causeTypes["0"]["_class"]) {
+          causeDetails['_primary'] = [
+            UpStreamProject : c.getUpstreamProject(),
+            UpstreamBuild : c.getUpstreamBuild(),
+            UpSreamUrl : c.getUpstreamUrl()
+          ];
+          causeList.add(causeDetails);
+          if()
           
         }
         
