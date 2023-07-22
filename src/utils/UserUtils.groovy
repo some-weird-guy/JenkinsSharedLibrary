96a56122df22d27cd0290d011d3946516f6e65be
KAPIL KUMAR <78157878+some-weird-guy@users.noreply.github.com>
2023-07-22 13:09:20 +0530

    Update UserUtils.groovy

--- src/utils/UserUtils.groovy
+++ src/utils/UserUtils.groovy
@@ -1,1 +1,29 @@
 
+
+import hudson.tasks.Mailer
+import hudson.model.User
+
+class UserUtils {
+
+
+  UserUtils() {
+    
+  }
+
+  @NonCPS
+  public def _getUsermailFromUserId(String userId) {
+    def userObj = __getUserObjFromUserId(userId);
+    def mailUserPropertyObj = userObj.getProperty(Mailer.UserProperty.class);
+    if(mailUserPropertyObj.hasExplicitlyConfiguredAddress()){
+        return mailUserPropertyObj.getExplicitlyConfiguredAddress();
+    }
+    else{
+        return mailUserPropertyObj.getAddress();
+    }
+      
+  }
+  @NonCPS
+  public def __getUserObjFromUserId(String userId) {
+    return User.get(userId)
+    }
+}
